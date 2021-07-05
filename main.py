54ab2e371eba4929c7090c4332d34a3109eeb5d3
POOJA-TELAVANE <74729744+POOJA-TELAVANE@users.noreply.github.com>
2021-07-05 17:44:59 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+
+Indian=["samosa","dal","rice"]
+chinese=["egg role","pot sticker","fried rices"]
+Italian=["pasta","pizza"]
+dish=input("Enter the name of the dish:")
+
+if dish in Indian:
+    print("The dish is indian")
+elif dish in chinese:
+    print("the dish is chinese")
+else:
+    print("the dish is italian")               #elif statement
+
+
+
+
+
+
+
+
+
+
